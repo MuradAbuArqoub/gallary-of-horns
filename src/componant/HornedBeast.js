e043c35dc18da066cc7d16a26a0e58b7dfe59141
MuradAbuArqoub <muradaboarkob2000@gmail.com>
2021-09-13 01:12:20 +0300

    done with the first lab

---
+++ src/componant/HornedBeast.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class HornedBeast extends React.Component {
+  render(){
+    return(
+      <div>
+          
+      <h2>UniWhal</h2>
+      <p>A unicorn and a narwhal nuzzling their horns</p>
+      <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" alt="UniWhal" title = "yes beast" />
+      
+      <h2>Rhino Family</h2>
+      <p>Mother (or father) rhino with two babies</p>
+      <img src="https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80" alt="Rhino" title = "Rhino Family"/>
+
+     
+      </div>
+    )
+  }
+}
+
+export default HornedBeast;
